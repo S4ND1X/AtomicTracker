a53eafed3fa860faa701881b6d7b88f0c87f94dc
MathewLG <mathewlopezg@gmail.com>
2021-09-21 15:51:14 -0500

    Functional development of all activities

---
+++ app/src/main/java/mx/tec/atomictracker/Login.kt
@@ -0,0 +1,53 @@
+package mx.tec.atomictracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Login : AppCompatActivity() {
+
+    lateinit var email : EditText
+    lateinit var password : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        email = findViewById(R.id.login_mail)
+        password = findViewById(R.id.login_password)
+    }
+
+    fun login(view : View?){
+        Firebase.auth.signInWithEmailAndPassword(
+            email.text.toString(),
+            password.text.toString()).addOnCompleteListener(this){
+                if(it.isSuccessful){
+                    Log.d("FIREBASE", "Registro exitoso")
+                    homeActivity(null)
+
+
+                }else{
+                    Log.d("FIREBASE", "Registro fallido: ${it.exception?.message}")
+                    Toast.makeText(this, "User not found, try again or sign up", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+
+
+    public fun homeActivity(view : View?){
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+    }
+
+    public fun signupActivity(view : View?){
+        val intent = Intent(this, Registro::class.java)
+        startActivity(intent)
+    }
+}
