a09a9d7b0cc1a64590778dbab0b0e07f5481da2d
Jorge Sanchez <sandix390@gmail.com>
2021-09-25 14:47:40 -0500

    fix merge conflict before stash

---
+++ app/src/main/java/mx/tec/atomictracker/CreacionHabitoActivity.kt
@@ -0,0 +1,11 @@
+package mx.tec.atomictracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreacionHabitoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creacion_habito)
+    }
+}
